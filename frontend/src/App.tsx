1db5e37c8559da01fb6408143f789e1ff8b9df8a
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-05-21 12:32:35 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layout/Layout";
+import Register from "./pages/Register";
+
+const App = () =>{
+  return (
+    <Router>
+      <Routes>
+        <Route path ="/" element ={
+        <Layout>
+          <p>Home Page</p>
+        </Layout>
+      }>
+        
+      </Route>
+        <Route path="/search" element={
+          <Layout>
+            <p>Search Page</p>
+          </Layout>
+        }
+      />
+
+      <Route path="/register" element={<Layout>
+        <Register/>
+      </Layout>}/>
+        <Route path="*" element={<Navigate to = "/"/>}></Route>
+      </Routes>
+    </Router>
+  )
+}
+export default App
